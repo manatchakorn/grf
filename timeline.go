12b9021b87dc55f4c36748ca0c96bd653ff84923
Manatchakorn D <manatchakorn@gmail.com>
2018-08-22 10:21:29 +0000

    add background task support

--- timeline.go
+++ timeline.go
@@ -4,32 +4,92 @@ import (
 	"time"
 )
 
-type TimeLine struct {
+type timeline struct {
 	tsk_num  uint16
 	duration uint32
 	repeat   bool
 	t_slot   []*Task
 }
 
-func (tl *TimeLine) CreateRepeatTimeline(duration uint32) *TimeLine {
+type bgtimeline struct {
+	tsk_num  uint16
+	duration uint32
+	repeat   bool
+	t_slot   []*Task
+}
+
+var Timeline *timeline
+var BgTimeline *bgtimeline
+
+func CreateTimeline() *timeline {
+	if Timeline == nil {
+		Timeline = &timeline{}
+	}
+	return Timeline
+}
+
+func (tl *timeline) CreateRepeatTimeline(duration uint32) *timeline {
 	tl.duration = duration
 	tl.repeat = true
 	return tl
 }
 
-func (tl *TimeLine) AddTask(tsk *Task) *TimeLine {
+func (tl *timeline) AddTask(tsk *Task) *timeline {
 	tl.t_slot = append(tl.t_slot, tsk)
 	tl.tsk_num = tl.tsk_num + 1
 	return tl
 }
 
-func (tl *TimeLine) runAllTask() {
+func (tl *timeline) runAllTask() {
 	for i := 0; i < int(tl.tsk_num); i++ {
 		tl.t_slot[i].fn()
 	}
 }
 
-func (tl *TimeLine) Start() {
+func (tl *timeline) Start() {
+	if tl.repeat {
+		ticker := time.NewTicker(time.Duration(tl.duration) * time.Millisecond)
+		quit := make(chan struct{})
+		for {
+			select {
+			case <-ticker.C:
+				tl.runAllTask()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	} else {
+		tl.runAllTask()
+	}
+}
+
+func CreateBackgroundTimeline() *bgtimeline {
+	if BgTimeline == nil {
+		BgTimeline = &bgtimeline{}
+	}
+	return BgTimeline
+}
+
+func (tl *bgtimeline) CreateRepeatTimeline(duration uint32) *bgtimeline {
+	tl.duration = duration
+	tl.repeat = true
+	return tl
+}
+
+func (tl *bgtimeline) AddTask(tsk *Task) *bgtimeline {
+	tl.t_slot = append(tl.t_slot, tsk)
+	tl.tsk_num = tl.tsk_num + 1
+	return tl
+}
+
+func (tl *bgtimeline) runAllTask() {
+	for i := 0; i < int(tl.tsk_num); i++ {
+		go tl.t_slot[i].fn()
+	}
+}
+
+func (tl *bgtimeline) Start() {
 	if tl.repeat {
 		ticker := time.NewTicker(time.Duration(tl.duration) * time.Millisecond)
 		quit := make(chan struct{})
